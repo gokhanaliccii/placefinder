0da5b37b9518c1750819080da27559eff60fbf1f
gökhan <gokhan@gokhan-MacBook-Pro.local>
2017-02-05 17:13:36 +0200

    gps receiver is added

---
+++ app/src/main/java/com/gokhanaliccii/placefinder/events/RefreshLocationEvent.java
@@ -0,0 +1,8 @@
+package com.gokhanaliccii.placefinder.events;
+
+/**
+ * Created by gokhan on 05/02/17.
+ */
+
+public class RefreshLocationEvent {
+}
---
+++ app/src/main/java/com/gokhanaliccii/placefinder/utility/ViewUtility.java
@@ -0,0 +1,8 @@
+package com.gokhanaliccii.placefinder.utility;
+
+/**
+ * Created by gokhan on 05/02/17.
+ */
+
+public class ViewUtility {
+}
